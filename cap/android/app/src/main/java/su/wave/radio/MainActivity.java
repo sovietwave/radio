b5bdfd5bdfbcb217a804e53852c163809444d6f8
Dmitry Grouzdev <dmitry.grouzdev@gmail.com>
2025-08-06 00:28:24 +0300

    stream stop fix 2

--- cap/android/app/src/main/java/su/wave/radio/MainActivity.java
+++ cap/android/app/src/main/java/su/wave/radio/MainActivity.java
@@ -1,5 +1,20 @@
 package su.wave.radio;
 
+import android.content.Intent;
+import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        // Start the audio service for background playback
+        Intent serviceIntent = new Intent(this, AudioService.class);
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            startForegroundService(serviceIntent);
+        } else {
+            startService(serviceIntent);
+        }
+    }
+}
